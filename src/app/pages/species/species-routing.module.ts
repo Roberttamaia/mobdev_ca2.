0baac839467ea093c3bd28ed22ab7409a3b659f6
Roberttamaia <robertta.94@gmail.com>
2022-11-04 16:45:12 +0000

    changing roitine module

--- src/app/pages/species/species-routing.module.ts
+++ src/app/pages/species/species-routing.module.ts
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class SpeciesPageRoutingModule {}
+export class FilmsPageRoutingModule {}
